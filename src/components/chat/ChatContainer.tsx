4263fd0ef30e8f4b171764248ce945f7a4ded113
nathidd <nathi@doubledown.studio>
2025-05-26 15:43:56 +0200

    fix: ts error for files

--- src/components/chat/ChatContainer.tsx
+++ src/components/chat/ChatContainer.tsx
@@ -4,9 +4,10 @@ import MessageList, { Message } from "./MessageList";
 type ChatContainerProps = {
   messages: Message[];
   isTyping: boolean;
+  files?: File[];
 };
 
-const ChatContainer = ({ messages, isTyping }: ChatContainerProps) => {
+const ChatContainer = ({ messages, isTyping, files }: ChatContainerProps) => {
   return (
 <div className="h-full flex flex-col">
       <div className="flex-1 overflow-y-auto">
